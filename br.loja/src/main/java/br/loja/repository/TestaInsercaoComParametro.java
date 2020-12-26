9f9d1e4cf6eebf81de8d719625543f90e2f9e8a8
geovanisucci <geovanis@ciandt.com>
2020-12-26 15:29:56 -0300

    transação aplicada

--- br.loja/src/main/java/br/loja/repository/TestaInsercaoComParametro.java
+++ br.loja/src/main/java/br/loja/repository/TestaInsercaoComParametro.java
@@ -3,6 +3,7 @@ package br.loja.repository;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.mysql.cj.xdevapi.PreparableStatement;
@@ -13,43 +14,68 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-
 		try {
-			
 			ConnectionFactory connectionFactory = new ConnectionFactory();
 			Connection connection = connectionFactory.recuperarConexao();
-			
-			
-			
 			String sql = "Insert into produto (nome, descricao) values (?, ?)";
-			
 			PreparedStatement stm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-			stm.setString(1, "MacBook");
-			stm.setString(2, "MacBook Pro");
 			
-			stm.execute();
-			
-			ResultSet rs = stm.getGeneratedKeys();
-			
-			while (rs.next()) {
+			try {
 				
-				int id = rs.getInt(1);
+				connection.setAutoCommit(false);
 				
-				System.out.println("ID Gerado -> " + id);
+				adiciona("Teste", "Teste", connection, stm);
+				adiciona("Teste 2", "Teste 2", connection, stm);
+				
+				connection.commit();
+				
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				connection.rollback();
+			}
+			finally {
+				
+				stm.close();
+				if(!connection.isClosed()) {
+					connection.close();
+				}
 				
 			}
-			
-			
-			if(!connection.isClosed()) {
-				connection.close();
-			}
-			
-			
 		} catch (Exception e) {
 			// TODO: handle exception
+			e.printStackTrace();
 		}
+		
+		
 
+	}
+
+	private static void adiciona(String nome, String descricao, Connection connection, PreparedStatement stm) throws SQLException {
+		
+		
+		
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+		
+		stm.execute();
+		
+		
+		ResultSet rs = stm.getGeneratedKeys();
+		
+		while (rs.next()) {
+			
+			int id = rs.getInt(1);
+			
+			System.out.println("ID Gerado -> " + id);
+			
+		}
+		
+		
+		
+		
+		
 	}
 
 }
